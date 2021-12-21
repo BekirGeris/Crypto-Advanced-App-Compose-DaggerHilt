139049d0ff40fda836fd2eb7bff39e288ee3a429
bekir geriş <bekir.geris@dgpaysit.com>
2021-12-21 13:20:37 +0300

    +compose navigation

--- app/src/main/java/com/begers/cryptocrazy/MainActivity.kt
+++ app/src/main/java/com/begers/cryptocrazy/MainActivity.kt
@@ -7,32 +7,66 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.navArgument
+import androidx.navigation.compose.rememberNavController
 import com.begers.cryptocrazy.ui.theme.CryptoCrazyTheme
+import com.begers.cryptocrazy.view.CryptoDetailScreen
+import com.begers.cryptocrazy.view.CryptoListScreen
+import java.util.ArrayList
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             CryptoCrazyTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                val navController = rememberNavController()
+
+                NavHost(navController = navController, startDestination = "crypto_list_screen"){
+
+                    composable("crypto_list_screen"){
+                        //Crypto list screen
+                        CryptoListScreen(navController = navController)
+                    }
+
+                    composable("crypto_detail_screen/{cryptoId}/{cryptoPrice}", arguments = listOf(
+                        navArgument("cryptoId") {
+                            type = NavType.StringType
+                        },
+                        navArgument("cryptoPrice") {
+                            type = NavType.StringType
+                        }
+                    )) {
+                        val cryptoId = remember {
+                            it.arguments?.getString("cryptoId")
+                        }
+
+                        val cryptoPrice = remember {
+                            it.arguments?.getString("cryptoPrice")
+                        }
+
+                        //crypto detail screen
+                        CryptoDetailScreen(
+                            id = cryptoId ?: "",
+                            price = cryptoPrice ?: "",
+                            navController = navController)
+
+                    }
+
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     CryptoCrazyTheme {
-        Greeting("Android")
+
     }
 }
---
+++ app/src/main/java/com/begers/cryptocrazy/util/Constants.kt
@@ -0,0 +1,9 @@
+package com.begers.cryptocrazy.util
+
+object Constants {
+
+    const val BASE_URL = ""
+    const val API_KEY = ""
+    const val CALL_ATTRIBUTES = ""
+
+}
