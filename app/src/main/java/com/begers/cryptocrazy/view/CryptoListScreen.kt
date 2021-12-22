c252f867dfdd3a93f20421252b6a8c69ddaf9b0a
bekir geriş <bekir.geris@dgpaysit.com>
2021-12-22 10:16:33 +0300

    +compose recycler view

--- app/src/main/java/com/begers/cryptocrazy/view/CryptoListScreen.kt
+++ app/src/main/java/com/begers/cryptocrazy/view/CryptoListScreen.kt
@@ -2,17 +2,18 @@ package com.begers.cryptocrazy.view
 
 import android.widget.Space
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.text.BasicTextField
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.material.TextField
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.focus.onFocusChanged
@@ -24,6 +25,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import com.begers.cryptocrazy.model.CryptoListItem
 import com.begers.cryptocrazy.viewModel.CryptoListViewModel
 
 @Composable
@@ -31,7 +33,6 @@ fun CryptoListScreen(
     navController: NavController,
     viewModel: CryptoListViewModel = hiltViewModel()
 ) {
-
     Surface(
         color = MaterialTheme.colors.secondary,
         modifier = Modifier.fillMaxSize()
@@ -50,11 +51,16 @@ fun CryptoListScreen(
             )
             Spacer(modifier = Modifier.height(10.dp))
             //Search
-            SearchBar(hint = "Search", modifier = Modifier.fillMaxWidth().padding(16.dp)) {
-                //viewModel.searchCryptoList(it)
+            SearchBar(
+                hint = "Search", modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+            ) {
+                viewModel.searchCryptoList(it)
             }
             Spacer(modifier = Modifier.height(10.dp))
             //List
+            CryptoList(navController = navController)
         }
     }
 }
@@ -93,14 +99,113 @@ fun SearchBar(
                 }
         )
 
-        if (isHintDisplayed){
+        if (isHintDisplayed) {
             Text(
                 text = hint,
                 color = Color.LightGray,
-                modifier = Modifier.padding(horizontal = 20.dp, vertical = 12.dp))
+                modifier = Modifier.padding(horizontal = 20.dp, vertical = 12.dp)
+            )
         }
     }
-
-
-    
 }
+
+@Composable
+fun CryptoList(
+    navController: NavController,
+    viewModel: CryptoListViewModel = hiltViewModel()
+) {
+    val cryptoList by remember { viewModel.cryptoList }
+    val errorMessage by remember { viewModel.errorMessage }
+    val isLoading by remember { viewModel.isLoading }
+
+    CryptoListView(cryptos = cryptoList, navController = navController)
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier.fillMaxSize()
+    ) {
+
+        if (isLoading) {
+            CircularProgressIndicator(color = MaterialTheme.colors.primary)
+        }
+        if (errorMessage.isNotEmpty()) {
+            RetryView(error = errorMessage) {
+                viewModel.loadCryptos()
+            }
+        }
+
+    }
+}
+
+@Composable
+fun CryptoListView(cryptos: List<CryptoListItem>, navController: NavController) {
+    LazyColumn(contentPadding = PaddingValues(5.dp)) {
+        items(cryptos) { crypto ->
+            CryptoRow(navController = navController, crypto = crypto)
+        }
+    }
+}
+
+@Composable
+fun CryptoRow(navController: NavController, crypto: CryptoListItem) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(color = MaterialTheme.colors.secondary)
+            .clickable {
+                navController.navigate(
+                    "crypto_detail_screen/${crypto.currency}/${crypto.price}"
+                )
+            }) {
+
+        Text(
+            text = crypto.currency,
+            style = MaterialTheme.typography.h4,
+            modifier = Modifier.padding(2.dp),
+            fontWeight = FontWeight.Bold,
+            color = MaterialTheme.colors.primary
+        )
+
+        Text(
+            text = crypto.price,
+            style = MaterialTheme.typography.h5,
+            modifier = Modifier.padding(2.dp),
+            fontWeight = FontWeight.Bold,
+            color = MaterialTheme.colors.primaryVariant
+        )
+    }
+}
+
+@Composable
+fun RetryView(
+    error: String,
+    onRetry: () -> Unit
+) {
+    Column() {
+        Text(
+            text = error,
+            color = Color.Red,
+            fontSize = 20.sp
+        )
+        Spacer(modifier = Modifier.height(10.dp))
+        Button(
+            onClick = {
+                onRetry()
+            },
+            modifier = Modifier.align(Alignment.CenterHorizontally)
+        ) {
+            Text(text = "Retry")
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
